029bfaedb6ef87d4bf8e4215c5881e2397122613
darten125 <darten124@gmail.com>
2022-10-07 23:03:33 +0300

    Lab_1_Zhmurenko

---
+++ Lab 1/First_Lab/stackFunctions.h
@@ -0,0 +1,7 @@
+#pragma once
+
+box* push(List & ourList); // Добавление элемента в начало стека
+
+void pop(List & ourList); // Удаление элемента из начала стека
+
+bool isEmpty(List & ourList); // Проверка стека на пустоту
