9e796f01455523c53bec26e68e735a4835437fc1
Kotov Ilya <the3k27@gmail.com>
2022-10-08 14:26:59 +0300

    pull

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # Алгоритмы и структуры данных (гр. 1373)
 ## Формат MR для загрузки работ
 1. Название ветки: <фамилия>_<lab1/lab2/lab3/course>
@@ -7,3 +8,17 @@
     - Отчёт в формате .pdf
     - src/
         - Файлы с исходным кодом
+=======
+#### Examples
+``inputLine: 1 * ( 1 - 11 ``
+``result: AssertionError: Separator is missed in the input string``
+
+``inputLine: 5 * cos ( 1 ) + sin ( 3 ) * ( 1 + 2 )``
+``result: 51cos*3sin12+*+``
+
+``inputLine: 4 * ( cos ( 1 ) * 6 ) * 2 + ( sin ( 3 ) - 7 )``
+``result: 41cos6**2*3sin7-+``
+
+``inputLine: 4 * ( cos ( 1 ) * 6 ) * ( 2 + sin ( 3 ) - 7 )``
+``result: 41cos6**23sin+7-*``
+>>>>>>> 418ce95 (lab1 new)
---
+++ src/main.py
@@ -0,0 +1,47 @@
+from src.classes import Stack
+
+def sortStation(inputLine):
+    availableInput = ['*', '/', '+', '-', '^', 'sin', 'cos', '(', ')']
+    operatorsPriority = {'+': 0, '-': 0, '*': 1, '/': 1, '^': 2}
+    functions = ['sin', 'cos']
+    stack, result = Stack(), Stack()
+    inputLine = inputLine.split()
+    assert inputLine.count('(') == inputLine.count(')'), 'Separator is missed in the input string'
+
+    for token in inputLine:
+        assert token in availableInput or token.isdigit(), 'Incorrect symbol in the input string'
+
+        if token.isdigit():
+            result.push(token)
+
+        elif token in functions:
+            stack.push(token)
+
+        elif token in operatorsPriority:
+            while stack.size() > 0 and stack.top() in operatorsPriority and operatorsPriority.get(stack.top()) >= operatorsPriority.get(token):
+                result.push(stack.pop())
+            stack.push(token)
+
+        elif token == '(':
+            stack.push(token)
+
+        elif token == ')':
+            while stack.top() != '(':
+                result.push(stack.pop())
+            stack.pop()
+            if stack.top() in functions:
+                result.push(stack.pop())
+
+    while stack.size() > 0 and stack.top() in operatorsPriority:
+        result.push(stack.pop())
+
+    output = ''
+    result.reverse()
+    while result.size() > 0:
+        output += result.pop()
+    return output
+
+
+if __name__ == "__main__":
+    inputLine = '4 * ( cos ( 1 ) * 6 ) * ( 2 + sin ( 3 ) - 7 )'
+    print(sortStation(inputLine))
