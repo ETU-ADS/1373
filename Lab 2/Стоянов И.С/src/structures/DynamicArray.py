a05a6cc0ecc8ed34ebe890052fa7795f52887fe9
Bahr <bahr.bahrovish@gmail.com>
2022-10-25 16:09:25 +0300

    Inital commit

---
+++ Lab 2/Стоянов И.С/src/structures/DynamicArray.py
@@ -0,0 +1,86 @@
+class DynamicArray:
+    def __init__(self) -> None:
+        self._lenght = 0
+        self._capacity = 10
+        self._array = self._make__array(self._capacity)
+
+    def __len__(self) -> int:
+        return self._lenght
+
+    def __getitem__(self, index: int) -> any:
+        if index >= self._lenght:
+            raise IndexError('Index is out of range!')
+        return self._array[index]
+
+    def __setitem__(self, index: int, value):
+        if index >= self._lenght:
+            raise IndexError('Index is out of range!')
+        self._array[index] = value
+
+    def append(self, element: any) -> any:
+        if self._lenght == self._capacity:
+            self._resize(int(1.6 * self._capacity))
+        self._array[self._lenght] = element
+        self._lenght += 1
+        return element
+
+    def insert(self, index, element):
+
+        if index < 0 or index > self._lenght:
+            raise IndexError('Index is out of range!')
+
+        if self._lenght == self._capacity:
+            self._resize(int(1.6*self._capacity))
+
+        for i in range(self._lenght - 1, index - 1, -1):
+            self._array[i+1] = self._array[i]
+
+        self._array[index] = element
+        self._lenght += 1
+
+    def remove_at(self, index: int) -> any:
+        if self._lenght == 0:
+            raise IndexError('Index is out of range!')
+
+        if index < 0 or index >= self._lenght:
+            raise IndexError('Index is out of range!')
+
+        element = self._array[index]
+        if index == self._lenght - 1:
+            element = self._array[index]
+            self._array[index] = None
+            self._lenght -= 1
+            return element
+
+        for i in range(index, self._lenght - 1):
+            self._array[i] = self._array[i+1]
+        self._array[self._lenght-1] = None
+        self._lenght -= 1
+        return element
+
+    def __str__(self) -> str:
+        line = '['
+        for i in range(0, self._lenght - 1):
+            line += str(self[i]) + ', '
+        line += str(self[self._lenght - 1]) + ']'
+        return line
+
+    def _resize(self, new_capacity: int) -> None:
+        new_array = self._make__array(new_capacity)
+
+        for k in range(self._lenght):
+            new_array[k] = self._array[k]
+
+        self._array = new_array
+        self._capacity = new_capacity
+
+    @staticmethod
+    def _make__array(new_capacity: int) -> list[None]:
+        return [None] * new_capacity
+
+    def swap(self, i: int, j: int) -> None:
+        if i >= self._lenght or j >= self._lenght:
+            raise IndexError('Index is out of range!')
+        a = self._array[i]
+        self._array[i] = self._array[j]
+        self._array[j] = a
---
+++ Lab 2/Стоянов И.С/src/timsort.py
@@ -0,0 +1,67 @@
+from structures import DynamicArray
+
+
+def timsort(arr: DynamicArray):
+    n = len(arr)
+    # Шаг 0. Вычисление minrun
+    minrun = get_minrun(n)
+
+    # Шаг 1. Разбиение на подмассивы и их сортировка
+    for start in range(0, n, minrun):
+        end = min(start + minrun - 1, n - 1)
+        insertion_sort(arr, start, end)
+
+    # Шаг 2. Слияние
+    size = minrun
+    while size < n:
+        for left in range(0, n, 2 * size):
+            mid = min(n - 1, left + size - 1)
+            right = min((left + 2 * size - 1), (n - 1))
+            if mid < right:
+                merge(arr, left, mid, right)
+        size = 2 * size
+
+
+def insertion_sort(arr: DynamicArray, left: int, right: int):
+    for i in range(left + 1, right + 1):
+        j = i
+        while j > left and arr[j] < arr[j - 1]:
+            arr.swap(j, j - 1)
+            j -= 1
+
+
+def get_minrun(n: int) -> int:
+    r = 0
+    while n >= 64:
+        r |= n & 1
+        n >>= 1
+    return n + r
+
+
+def merge(arr: DynamicArray, l: int, m: int, r: int):
+    len1, len2 = m - l + 1, r - m
+    left, right = [], []
+    for i in range(0, len1):
+        left.append(arr[l + i])
+    for i in range(0, len2):
+        right.append(arr[m + 1 + i])
+
+    i, j, k = 0, 0, l
+    while i < len1 and j < len2:
+        if left[i] <= right[j]:
+            arr[k] = left[i]
+            i += 1
+        else:
+            arr[k] = right[j]
+            j += 1
+        k += 1
+
+    while i < len1:
+        arr[k] = left[i]
+        k += 1
+        i += 1
+
+    while j < len2:
+        arr[k] = right[j]
+        k += 1
+        j += 1
