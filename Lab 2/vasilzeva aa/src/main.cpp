54f9f021cc15d6e56dc10402e2d3f86fa53cddcc
AgniiaVasilzeva <agnija.vasilzeva@mail.ru>
2022-11-02 21:48:43 +0300

    final ver

--- Lab 2/vasilzeva aa/src/main.cpp
+++ Lab 2/vasilzeva aa/src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <chrono>
+
 
 #include "randNum.h"
 #include "myStack.h"
@@ -18,9 +20,11 @@ int main() {
     randomNumbers(minValue, maxValue, N, DATA_arr);
     std::cout << "For rand numbers (in amount of "<< N << ") from " << minValue << " to " << maxValue << " your sort array is:\n\n";
 
-    unsigned int start_time = clock();
-    Teamsort(DATA_arr, N);
-    unsigned int end_time = clock();
+    //unsigned int start_time = clock();
+    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+    Timsort(DATA_arr, N);
+    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+    //unsigned int end_time = clock();
 
     int prevElement = -1;
     for (int i = 0; i < N; i++) {
@@ -34,7 +38,9 @@ int main() {
         std::cout << DATA_arr[i] << ' ';
     }
 
-    std::cout << "\n\nWork time: " << end_time - start_time << " ms";
+    std::cout << "\n\nWork time: " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << " us";
+
+    //std::cout << "\n\nWork time: " << end_time - start_time << " ms";
 
     std::cout << '\n';
     system("pause");
