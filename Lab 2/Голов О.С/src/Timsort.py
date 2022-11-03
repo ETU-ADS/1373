2f86a03e5302360b145facfe32b389312da328a4
avariiinaa <log7548@gmail.com>
2022-11-04 02:08:01 +0300

    доработано вообще всё
    оп, + галоп. кек, переработан стек, а также повышение эффективности функций

--- Lab 2/Голов О.С/src/Timsort.py
+++ Lab 2/Голов О.С/src/Timsort.py
@@ -15,23 +15,27 @@ def bin_search(array, n) -> int:
         low = 0
         high = len(array) - 1
         mid = high // 2
+        if n >= array[-1]:
+            return len(array)
         while low != high:
             if array[mid] <= n <= array[mid + 1]:
                 return mid + 1
+            elif n < array[mid]:
+                high = mid
+                mid = (low + high) // 2
             elif n > array[mid]:
                 low = mid
                 mid = (low + high) // 2
 
-            elif n < array[mid]:
-                high = mid
-                mid = (low + high) // 2
-
             elif n == array[mid]:
                 return mid
+
     else:
         high = 0
         low = len(array) - 1
         mid = low // 2
+        if n <= array[-1]:
+            return len(array)
         while low != high:
             if array[mid] >= n >= array[mid + 1]:
                 return mid + 1
@@ -51,14 +55,15 @@ def bin_search(array, n) -> int:
 
 def ins_sort(array, start, end):
     exp = array[end]
-    point = bin_search(array[start:end], array[end])
-    if array[start] <= array[end - 1]:
-        for i in range(end, start + point, -1):
-            array[i] = array[i - 1]
+    if array[start] <= array[end - 1]:  # 1 2 3 4 2
+        while end != start and array[end - 1] >= exp:
+            array[end] = array[end - 1]
+            end -= 1
     else:
-        for i in range(end, start + point, -1):
-            array[i] = array[i - 1]
-    array[start + point] = exp
+        while end != start and array[end - 1] <= exp:
+            array[end] = array[end - 1]
+            end -= 1
+    array[end] = exp
 
 
 def reversing(array, start, end):
@@ -89,28 +94,25 @@ def merging(array, left, mid, right):
             rgallop_flag = 0
             tmparr[k] = array[lp]
             lgallop_flag += 1
-            if lgallop_flag == 7:  # gallop
-                gal = 1
-                while lp + gal * 2 < mid - left and array[lp + gal * 2] < array[rp]:
-                    gal *= 2
-                for i in range(1, gal + 1):
-                    tmparr[k + i] = array[lp + i]
-                k += gal - 1
-                lp += gal - 1
             lp += 1
+            if lgallop_flag == 7:  # gallop
+                gal = bin_search(array[left:mid], array[rp])
+                while lp < gal:
+                    k += 1
+                    tmparr[k] = array[lp]
+                    lp += 1
+
         else:
             lgallop_flag = 0
             tmparr[k] = array[rp]
             rgallop_flag += 1
-            if rgallop_flag == 7:  # gallop too
-                gal = 1
-                while rp + gal * 2 < right - mid + 1 and array[rp + gal * 2] <= array[lp]:
-                    gal *= 2
-                for i in range(1, gal + 1):
-                    tmparr[k + i] = array[rp + i]
-                k += gal - 1
-                rp += gal - 1
             rp += 1
+            if rgallop_flag == 7:  # gallop too
+                gal = bin_search(array[mid:right + 1], array[lp])
+                while rp < gal:
+                    k += 1
+                    tmparr[k] = array[rp]
+                    rp += 1
         k += 1
     for k in range(left, right + 1):
         array[k] = tmparr[k - left]
@@ -124,39 +126,41 @@ def timsort(array) -> list:
     while rstart + 1 < n:  # run maker
         rpos = 0
         if array[rstart] <= array[rstart + 1]:  # 1 2 3 4
-            while (rpos <= minrun or array[rpos + rstart] <= array[rstart + 1]) and rpos + rstart + 1 < n:
+            while rpos + rstart + 1 < n and (rpos <= minrun or array[rpos + rstart] <= array[rstart + rpos + 1]):
                 rpos += 1
                 if array[rpos + rstart - 1] >= array[rpos + rstart]:
                     ins_sort(array, rstart, rpos + rstart)
         else:  # 4 3 2 1
-            while (rpos <= minrun or array[rpos + rstart] >= array[rstart + 1]) and rpos + rstart + 1 < n:
+            while rpos + rstart + 1 < n and (rpos <= minrun or array[rpos + rstart] >= array[rstart + rpos + 1]):
                 rpos += 1
                 if array[rpos + rstart - 1] <= array[rpos + rstart]:
                     ins_sort(array, rstart, rpos + rstart)
             reversing(array, rstart, rpos + rstart)
-        if len(runs) == 2:  # stack imitator pt. 1
-            if runs[-2][1] <= runs[-1][1]:
-                merging(array, runs[-2][0], runs[-1][0], sum(runs[-1]))
-                runs[-2][1] += runs[-1][1] + 1  #
-                runs.pop(-1)  #
         runs.append([rstart, rpos])
-        if len(runs) > 2:  # stack imitator pt. 2
-            if runs[-3][1] <= runs[-2][1] + runs[-1][1]:
-                if runs[-3][1] > runs[-1][1]:
-                    merging(array, runs[-2][0], runs[-1][0], sum(runs[-1]))
-                    runs[-2][1] += runs[-1][1] + 1  #
-                    runs.pop(-1)  #
-                else:
-                    merging(array, runs[-3][0], runs[-2][0], sum(runs[-2]))
-                    runs[-3][1] += runs[-2][1] + 1  #
-                    runs.pop(-2)  #
-            else:
-                merging(array, runs[-2][0], runs[-1][0], sum(runs[-1]))
-                runs[-2][1] += runs[-1][1] + 1  #
-                runs.pop(-1)  #
         rstart += rpos + 1
-    if len(runs) > 1:
-        merging(array, runs[-2][0], runs[-1][0], sum(runs[-1]))
+    while len(runs) > 1:
+        speci = 0
+        if len(runs) == 2:
+            merging(array, runs[0][0], runs[1][0], sum(runs[1]))
+            return array
+        while len(runs) > 2 + speci:
+            if runs[0 + speci][1] <= runs[1 + speci][1] + runs[2 + speci][1] and runs[1 + speci][1] <= runs[2 + speci][1]:  
+                merging(array, runs[1 + speci][0], runs[2 + speci][0], sum(runs[2 + speci]))
+                runs[1 + speci][1] += runs[2 + speci][1] + 1  #
+                runs.pop(2 + speci)  #
+                merging(array, runs[0 + speci][0], runs[1 + speci][0], sum(runs[1 + speci]))
+                runs[0 + speci][1] += runs[1 + speci][1] + 1  #
+                runs.pop(1 + speci)  #
+            else:
+                if runs[0 + speci][1] <= runs[1 + speci][1]:
+                    merging(array, runs[0 + speci][0], runs[1 + speci][0], sum(runs[1 + speci]))
+                    runs[0 + speci][1] += runs[1 + speci][1] + 1  #
+                    runs.pop(1 + speci)
+                    speci += 1
+                else:
+                    merging(array, runs[1 + speci][0], runs[2 + speci][0], sum(runs[2 + speci]))
+                    runs[1 + speci][1] += runs[2 + speci][1] + 1  #
+                    runs.pop(2 + speci)
     return array
 
 
