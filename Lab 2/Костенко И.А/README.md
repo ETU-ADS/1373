8faa997386796ede367c7d9f8332f8a717121921
rolanos <ivankson@gmail.com>
2022-11-05 17:33:55 +0300

    1. Kostenko_lab2
    -Сортировка вставками
    -Сортировка слиянием
    -Определение minrun
    -Режим галопа
    Это использовал в реализации Timsort сортировке

---
+++ Lab 2/Костенко И.А/README.md
@@ -0,0 +1,2 @@
+# Second laboratory work [WIP]
+Second laboratory work for the discipline "Algorithms and Data Structures" in group 1373
---
+++ Lab 2/Костенко И.А/src/lab2/lab2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "../lab2/List.h"
+
+int main()
+{
+	int buffer[18] = { 11, 10, 12, 13, 14,15,16,17,18,1,2,3,5,101,202,55,44, 37};
+	ArrayList<int> arr = ArrayList<int>();
+	arr.insertArray(buffer, 18);
+	arr.add(1);
+	arr.add(0);
+	arr.print();
+	cout << "\n\n\n";
+	arr.timsort();
+	system("pause");
+	return 0;
+}
+
