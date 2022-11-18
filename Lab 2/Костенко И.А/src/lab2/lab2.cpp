9cec29f4c5f76e5c6ed0051b74ef3724e8c3dd78
rolanos <112723225+rolanos@users.noreply.github.com>
2022-11-19 00:53:50 +0300

    Kostenko lab 2 (#28)
    * 1. Kostenko_lab2
    
    -Сортировка вставками
    -Сортировка слиянием
    -Определение minrun
    -Режим галопа
    Это использовал в реализации Timsort сортировке
    
    * Update README.md
    
    * 2. Доработки
    
    -Добавил проверку снятия последовательностей со стека (XYZ проверка).
    -Исправил отчёт по работе, добавив в него более точный материал и больше рисунков.
    -Разрешил проблему с кодировкой файлов.
    -Расширил материал в README.md и разрешил конфликт README.md файлов.
    -Удалил отчёт из пулл реквеста.
    
    * 3.Разрешение конфликта README.md
    
    Вернул README.md

---
+++ Lab 2/Костенко И.А/src/lab2/lab2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "../lab2/List.h"
+
+int main()
+{
+	ArrayList<int> arr = ArrayList<int>();
+	for (int i = 0; i < 200; i++)
+	{
+		int index = rand() % 100;
+		arr.add(index);
+	}
+	arr.print();
+	cout << "\n\n\n";
+	arr.timsort();
+	system("pause");
+	return 0;
+}
+
