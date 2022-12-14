0743970c7145efb4677a0abafb3a72a2a0a2bb6b
NoBadWord <ruslanwork2110@mail.ru>
2022-12-15 01:14:37 +0300

    Commit 1

---
+++ Course work/Хоменко Р Д/src/main.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <windows.h>
+#include "Graph.h"
+
+int main()
+{
+    Graph graph;
+    std::cout << "GRAPH" << std::endl;
+    graph.read("Graph.txt");
+    std::cout << "\nDFS: ";
+    graph.dfs();
+    std::cout << std::endl;
+    std::cout << "\nBFS: ";
+    graph.bfs();
+    std::cout << "\nKRASKAL" << std::endl;
+    graph.kraskal();
+    system("pause");
+    return 0;
+}
