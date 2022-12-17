e0193581573c18031404de04a37aba2be889ff8e
bahr <bahrwork0@gmail.com>
2022-12-17 18:51:55 +0300

    add README.md

--- Course work/Stoyanov/src/main.py
+++ Course work/Stoyanov/src/main.py
@@ -8,21 +8,21 @@ def main():
     # 0  3  1
     # 3  0  2
     # 1  2  0
-    # edges = [
-    #     Edge(3, 'A', 'B'),
-    #     Edge(1, 'A', 'C'),
-    #     Edge(2, 'B', 'C')
-    # ]
-    # cost, res = find_spanning_tree(edges)
-    # for r in res:
-    #     print(f'{r.a} {r.b}')
-    # print(cost)
-    # print(f'Example 1: {cost=}')
+    edges = [
+        Edge(3, 'A', 'B'),
+        Edge(1, 'A', 'C'),
+        Edge(2, 'B', 'C')
+    ]
+    cost, res = find_spanning_tree(edges)
+    for r in res:
+        print(f'{r.a} {r.b}')
+    print(cost)
+    print(f'Example 1: {cost=}')
 
     # Example 2: Weird.txt
     edges = read_matrix('data/Weird.txt')
     cost, res = find_spanning_tree(edges)
-    # print(f'Example 2: {cost=}, {res=}')
+    
     for r in res:
         print(f'{r.a} {r.b}')
     print(cost)
