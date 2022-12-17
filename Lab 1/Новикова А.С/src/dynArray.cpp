a8c4c499fe6e372340e99057910427fadaf76c3e
Анна <asnovikova03@gmail.com>
2022-12-17 07:12:31 +0500

    Update dynArray.cpp

--- Lab 1/Новикова А.С/src/dynArray.cpp
+++ Lab 1/Новикова А.С/src/dynArray.cpp
@@ -2,19 +2,35 @@
 
 DynArray::DynArray()
 {
-	size = 1;
-	data = new std::string[size];
+	capacity = 1;
+	size = 0;
+	data = new std::string[capacity];
 }
 
 DynArray::DynArray(int elemAmount)
 {
-	size = elemAmount;
-	data = new std::string[size];
+	capacity = elemAmount;
+	size = 0;
+	data = new std::string[capacity];
 }
 
 DynArray::~DynArray()
 {
 	delete[] data;
+}
+
+void DynArray::expand()
+{
+	capacity *= 2;
+
+	std::string* temp = new std::string[capacity];
+
+	for (int i = 0; i < size; i++)
+		temp[i] = data[i];
+
+	delete[] data;
+	data = temp;
+
 }
 
 int DynArray::find(std::string value)
@@ -30,41 +46,42 @@ int DynArray::find(std::string value)
 
 void DynArray::add(int index, std::string value)
 {
-	if (index >= 0)
+	if (index <= size)
 	{
-		if (index < size) 
+		if (index == size)
+			pushBack(value);
+		else
+		{
+			if (size >= capacity)
+				expand();
+
+			for (int i = 0; i < size - index; i++)
+				data[size - i] = data[size - i - 1];
+
 			data[index] = value;
+			size++;
+		}
 	}
 	else
-	{
-		std::cout << "Incorrect index";
-	}
+		std::cout << "Incorrect index.\n";
 }
 
 void DynArray::remove(int index)
 {
-	if (index >= 0)
+	if (index >= 0 && index < size)
 	{
 		if (index == size - 1)
-		{
 			size--;
-		}
-		else if (index < size - 1)
+		else 
 		{
-			for (int i = index; i < size - 1; i++)
+			for (int i = index; i < size; i++)
 				data[i] = data[i + 1];
 
 			size--;
 		}
-		else
-		{
-			std::cout << "Nothing to remove";
-		}
 	}
 	else
-	{
 		std::cout << "Incorrect index";
-	}
 }
 
 int DynArray::getSize()
@@ -74,17 +91,14 @@ int DynArray::getSize()
 
 void DynArray::pushBack(std::string value)
 {
-	std::string* temp = new std::string[size + 1];
-	for (int i = 0; i < size; i++)
-		temp[i] = data[i];
+	if (size >= capacity)
+		expand();
 
-	temp[size++] = value;
-
-	delete[] *&data;
-	data = temp;
+	data[size++] = value;
 }
 
 std::string& DynArray::operator[](const int index)
 {
-	return data[index];
+	if (index >= 0 && index < size)
+		return data[index];
 }
