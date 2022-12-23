96cffbee26d57a5d591874d9e50c21199e37c703
Timur141234 <36606070+Timur141234@users.noreply.github.com>
2022-12-23 22:29:07 +0300

    Takanaev_lab1 (#50)

---
+++ Lab 1/Таканаев Т.Ш/src/ru/Timur141234/ShuntingYard/OperatorBinary.java
@@ -0,0 +1,11 @@
+package ru.Timur141234.ShuntingYard;
+
+/**
+ *
+ * @author Timur Takanaev
+ */
+public interface OperatorBinary {
+
+    double apply(double a, double b);
+
+}
---
+++ Lab 1/Таканаев Т.Ш/src/ru/Timur141234/Structures/LinkedList/Node.java
@@ -0,0 +1,23 @@
+package ru.Timur141234.Structures.LinkedList;
+
+/**
+ *
+ * @author Timur Takanaev
+ */
+public class Node {
+
+    public int data;
+    public Node next;
+
+    public Node() {
+
+    }
+
+    public Node(int data) {
+
+        this.data = data;
+        this.next = null;
+
+    }
+
+}
